d7f0c30ab95221194fdd20e0312322e241f8e23f
J9pro <janine239@gmail.com>
2023-01-20 20:40:43 +0100

    added

--- game.js
+++ game.js
@@ -1,24 +1,50 @@
 const boxes = Array.from(document.getElementsByClassName("box"))
+//console.log(boxes)
 const button = document.querySelector("#btn")
-
-
 
 let currentPlayer = 'x'
 let player2 = 'O'
 let spaces = Array(9).fill(null)
+//console.log(spaces)
 
-console.log(boxes)
+
+
+button.addEventListener('click', resetBox)
 
 boxes.forEach(box => {
     box.addEventListener('click', fillbox)
 })
 
-function fillbox(e){
-    const id = e.target.id
-if(!spaces[id]){
-    spaces[id] = currentPlayer
-    e.target.innerText = currentPlayer
-}else{
-    player2 = "O"
-}}
+function fillbox(e) {
+    const id = e.target.id;
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+    }
+    changePlayer();
+}
+
+function changePlayer() {
+    if (currentPlayer == 'x') {
+        currentPlayer = 'o'
+    } else {
+        currentPlayer = 'x'
+    }
+}
+
+//reset button
+
+function resetBox() {
+    boxes.forEach(box => {
+        box.innerHTML = ''
+        console.log('helo')
+    })
+}
+
+//Problems
+
+//once I click Reset for the second time the button does not reset the game.
+
+//we need to add the winning conditions
 
